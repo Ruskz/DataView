a48c5c0324ae7935faf813cd28ad7cc88bed1ad1
Austin <amelville94@gmail.com>
2014-11-20 21:55:59 -0500

    Completed map.py and added data files

--- map.py
+++ map.py
@@ -13,8 +13,6 @@ import geojson as g
 from parse import parse, MY_FILE
 import parse as p
 
-print MY_FILE
-
 def create_map(data_file):
     # Define type of GeoJSON we're creating
     geo_map = {"type": "FeatureCollection"}
@@ -22,33 +20,48 @@ def create_map(data_file):
     # Define empty list to collect each point to graph
     item_list = []
 
-    # Iterate oveer out data to create GeoJSON document
-    # We're using enumerate() so we fet the line, as well
+    # Iterate over our data to create GeoJSOn document.
+    # We're using enumerate() so we get the line, as well
     # the index, which is the line number.
     for index, line in enumerate(data_file):
 
-        # Skip any zero coordinates as this will through off
+        # Skip any zero coordinates as this will throw off
         # our map.
-        if line['X'] == '0' or line['Y'] == '0':
+        if line['X'] == "0" or line['Y'] == "0":
             continue
 
-        # Setup a new dictionary for each iteration
+        # Setup a new dictionary for each iteration.
         data = {}
 
-        # Assign line items to appropriate GeoJSON fields
+        # Assign line items to appropriate GeoJSON fields.
         data['type'] = 'Feature'
         data['id'] = index
         data['properties'] = {'title': line['Category'],
-                              'description': line['Descript'].
+                              'description': line['Descript'],
                               'date': line['Date']}
         data['geometry'] = {'type': 'Point',
                             'coordinates': (line['X'], line['Y'])}
 
         # Add data dictionary to our item_list
+        item_list.append(data)
 
+    # For each point in our item_list, we add the point to our
+    # dictionary.  setdefault creates a key called 'features' that
+    # has a value type of an empty list.  With each iteration, we
+    # are appending our point to that list.
+    for point in item_list:
+        geo_map.setdefault('features', []).append(point)
 
+    # Now that all data is parsed in GeoJSON write ti a file so we
+    # can upload it to gist.github.com
+    with open('data/file_sf.geojson', 'w') as f:
+        f.write(g.dumps(geo_map))
 
+def main():
+    data = parse(MY_FILE, ',')
 
+    return create_map(data)
 
-
-
+if __name__ == "__main__":
+    main()
+                
